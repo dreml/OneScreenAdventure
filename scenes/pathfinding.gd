a958b02b070afecc64fe82f5d60787bc5c567975
dreml <nik.dreml@gmail.com>
2024-01-14 13:39:34 +0400

    upgrade to godot 4

--- scenes/pathfinding.gd
+++ scenes/pathfinding.gd
@@ -1,26 +1,26 @@
 extends TileMap
 
-const BASE_LINE_WIDTH = 3.0
-const DRAW_COLOR = Color.white
+const BASE_LINE_WIDTH: float = 3.0
+const DRAW_COLOR: Color = Color.WHITE
 
-export(Vector2) var map_size = Vector2.ONE * 16
-export var pointer_path: NodePath
+@export var map_size: Vector2 = Vector2.ONE * 16
+@export var pointer_path: NodePath
 
-var path_start_position = Vector2() setget _set_path_start_position
-var path_end_position = Vector2() setget _set_path_end_position
+var path_start_position: Vector2i = Vector2i(): set = _set_path_start_position
+var path_end_position: Vector2i = Vector2i(): set = _set_path_end_position
 
 var _point_path = []
+var cell_size: int = 64
 
-onready var astar_node = AStar.new()
-onready var _half_cell_size = cell_size / 2
-onready var pointer = get_node(pointer_path)
+@onready var astar_node: AStar2D = AStar2D.new()
+@onready var pointer: Node2D = get_node(pointer_path)
 
 # TODO разделить преграды на разные атласы и получать занятые клетки по id тайлсета
-onready var obstacles = get_used_cells() 
+@onready var obstacles: Array[Vector2i] = get_used_cells(1) 
 
 func _ready():
 	calculate_obstacles()
-	var walkable_cells_list = astar_add_walkable_cells()
+	var walkable_cells_list: Array[Vector2i] = astar_add_walkable_cells()
 	astar_connect_walkable_cells_diagonal(walkable_cells_list)
 	
 
@@ -29,31 +29,33 @@ func _ready():
 #		return
 #
 #	var point_start = _point_path[0]
-#	var last_point = map_to_world(Vector2(point_start.x, point_start.y)) + _half_cell_size
+#	var last_point = map_to_world(Vector2(point_start.x, point_start.y))
 #	for index in range(1, len(_point_path)):
-#		var current_point = map_to_world(Vector2(_point_path[index].x, _point_path[index].y)) + _half_cell_size
+#		var current_point = map_to_world(Vector2(_point_path[index].x, _point_path[index].y))
 #		draw_line(last_point, current_point, DRAW_COLOR, BASE_LINE_WIDTH, true)
 #		draw_circle(current_point, BASE_LINE_WIDTH * 2.0, DRAW_COLOR)
 #		last_point = current_point
 
 func calculate_obstacles() -> void:
 	for obstacle in get_tree().get_nodes_in_group('obstacles'):
-		var obstacle_cell_pos = obstacle.position / cell_size
-		var obstacle_cell = Vector2(floor(obstacle_cell_pos.x), floor(obstacle_cell_pos.y))
+		var obstacle_cell_pos = obstacle.global_position / cell_size
+		var obstacle_cell: Vector2i = Vector2i(floor(obstacle_cell_pos.x), floor(obstacle_cell_pos.y))
 		if not obstacle_cell in obstacles:
 			obstacles.append(obstacle_cell)
 
-func astar_add_walkable_cells():
-	var points_array = []
+func astar_add_walkable_cells() -> Array[Vector2i]:
+	print(get_layer_name(1))
+	
+	var points_array: Array[Vector2i] = []
 	for x in range(map_size.x):
 		for y in range(map_size.y):
-			var point = Vector2(x, y)
+			var point: Vector2i = Vector2i(x, y)
 			if point in obstacles:
 				continue
 
 			points_array.append(point)
 			var point_index = calculate_point_index(point)
-			astar_node.add_point(point_index, Vector3(point.x, point.y, 0.0))
+			astar_node.add_point(point_index, point)
 			
 	return points_array
 
@@ -62,7 +64,7 @@ func astar_connect_walkable_cells_diagonal(points_array):
 		var point_index = calculate_point_index(point)
 		for local_x in range(3):
 			for local_y in range(3):
-				var point_relative = Vector2(point.x + local_x - 1, point.y + local_y - 1)
+				var point_relative: Vector2i = Vector2i(point.x + local_x - 1, point.y + local_y - 1)
 				var point_relative_index = calculate_point_index(point_relative)
 				if point_relative == point or is_outside_map_bounds(point_relative):
 					continue
@@ -73,39 +75,39 @@ func astar_connect_walkable_cells_diagonal(points_array):
 func calculate_point_index(point):
 	return point.x + map_size.x * point.y
 
-func clear_previous_path_drawing():
-	if not _point_path:
-		return
-	var point_start = _point_path[0]
-	var point_end = _point_path[len(_point_path) - 1]
-	set_cell(point_start.x, point_start.y, -1)
-	set_cell(point_end.x, point_end.y, -1)
+#func clear_previous_path_drawing() -> void:
+#	if not _point_path:
+#		return
+#	var point_start = _point_path[0]
+#	var point_end = _point_path[len(_point_path) - 1]
+	#set_cell(point_start.x, point_start.y, -1)
+	#set_cell(point_end.x, point_end.y, -1)
 	
 	
 func is_outside_map_bounds(point):
 	return point.x < 0 or point.y < 0 or point.x >= map_size.x or point.y >= map_size.y
 	
-func get_astar_path(world_start, world_end):
-	self.path_start_position = world_to_map(world_start)
-	self.path_end_position = world_to_map(world_end)
+func get_astar_path(world_start, world_end) -> Array[Vector2]:
+	self.path_start_position = local_to_map(world_start)
+	self.path_end_position = local_to_map(world_end)
 	_recalculate_path()
-	var path_world = []
+	var path_world: Array[Vector2] = []
 	for point in _point_path:
-		var point_world = map_to_world(Vector2(point.x, point.y)) + _half_cell_size
+		var point_world: Vector2 = map_to_local(Vector2(point.x, point.y)) 
 		path_world.append(point_world)
 		
-	pointer.position = map_to_world(self.path_end_position) + _half_cell_size
+	pointer.global_position = map_to_local(self.path_end_position)
 	
 	return path_world
 
 func _recalculate_path():
-	clear_previous_path_drawing()
+#	clear_previous_path_drawing()
 	var start_point_index = calculate_point_index(path_start_position)
 	var end_point_index = calculate_point_index(path_end_position)
 	_point_path = astar_node.get_point_path(start_point_index, end_point_index)
-	update()
+	#update()
 
-func _set_path_start_position(value):
+func _set_path_start_position(value) -> void:
 	if is_outside_map_bounds(value):
 		return
 	if value in obstacles:
@@ -115,7 +117,7 @@ func _set_path_start_position(value):
 	if path_end_position and path_end_position != path_start_position:
 		_recalculate_path()
 
-func _set_path_end_position(value):
+func _set_path_end_position(value) -> void:
 	if is_outside_map_bounds(value):
 		return
 	if value in obstacles:
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,31 +1,29 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
 enum States { IDLE, FOLLOW }
 
-const MASS = 10.0
-const ARRIVE_DISTANCE = 5.0
+const MASS: float = 10.0
+const ARRIVE_DISTANCE: float = 5.0
 
-export(float) var speed = 200.0
-export var nav_path: NodePath
-export var pointer_path: NodePath
+@export var speed: float = 200.0
+@export var nav_path: NodePath
+@export var pointer_path: NodePath
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var nav: TileMap = get_node(nav_path)
+@onready var pointer: Node2D = get_node(pointer_path)
 
-var _state = States.IDLE
+var _state: int = States.IDLE
 
 var _path = []
-var _target_point_world = Vector2()
-var _target_position = Vector2()
-var _velocity = Vector2()
-
-onready var sprite = $AnimatedSprite
-onready var agent = $NavigationAgent2D
-onready var nav: TileMap = get_node(nav_path)
-onready var pointer: Node2D = get_node(pointer_path)
+var _target_point_world: Vector2 = Vector2()
+var _target_position: Vector2 = Vector2()
+var _velocity: Vector2 = Vector2()
 
 func _ready():
 	_change_state(States.IDLE)
 	
-func _process(_delta):
+func _process(_delta) -> void:
 	if _state != States.FOLLOW:
 		return
 		
@@ -34,9 +32,9 @@ func _process(_delta):
 	elif _target_point_world.x - self.position.x > 5:
 		sprite.flip_h = false
 	
-	var _arrived_to_next_point = _move_to(_target_point_world)
+	var _arrived_to_next_point: bool = _move_to(_target_point_world)
 	if _arrived_to_next_point:
-		_path.remove(0)
+		_path.remove_at(0)
 		if len(_path) == 0:
 			_change_state(States.IDLE)
 			return
@@ -44,7 +42,7 @@ func _process(_delta):
 	
 func _unhandled_input(event):
 	if event.is_action_pressed("click"):
-		var global_mouse_pos = get_global_mouse_position()
+		var global_mouse_pos: Vector2 = get_global_mouse_position()
 		if Input.is_key_pressed(KEY_SHIFT):
 			global_position = global_mouse_pos
 		else:
@@ -52,7 +50,7 @@ func _unhandled_input(event):
 		
 		_change_state(States.FOLLOW)
 		
-func _move_to(world_position):
+func _move_to(world_position) -> bool:
 	var desired_velocity = (world_position - position).normalized() * speed
 	var steering = desired_velocity - _velocity
 	_velocity += steering / MASS
@@ -60,7 +58,7 @@ func _move_to(world_position):
 	return position.distance_to(world_position) < ARRIVE_DISTANCE
 
 
-func _change_state(new_state):
+func _change_state(new_state) -> void:
 	if new_state == States.FOLLOW:
 		sprite.play('run')
 		
