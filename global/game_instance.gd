ac89a9cd81b053f3f6f4dc396abfc0a3c80161fc
dreml <nik.dreml@gmail.com>
2024-02-29 12:30:00 +0400

    доработка game instance + исправление крашей ios (#11)

--- global/game_instance.gd
+++ global/game_instance.gd
@@ -1,4 +1,10 @@
 extends Node
 
+@onready var player: Player = get_tree().get_root().get_node('Main/Player') 
+
+var wood_amount := 0
+var gold_amount := 0
+var meat_amount := 0
+
 func _ready() -> void:
 	pass
--- player/player.gd
+++ player/player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
 enum States { IDLE, FOLLOW, ATTACK }
