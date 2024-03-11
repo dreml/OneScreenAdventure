48966e9882cf9923f29f8925ac315cc9ffd14837
dreml <nik.dreml@gmail.com>
2024-03-11 15:15:33 +0400

    настроил movement component для работы с новой навигацией

--- scenes/game_director.gd
+++ scenes/game_director.gd
@@ -5,7 +5,6 @@ extends Node2D
 
 @onready var attack_timer: Timer = $AttackTimer
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 #	attack_timer.start()
 	attack_timer.timeout.connect(move_goblins)
@@ -13,9 +12,7 @@ func _ready():
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_T):
 		attack_timer.start()
-	
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	pass
 
