01e26d487b78eb049ac2a776c3f211443bf559da
dreml <nik.dreml@gmail.com>
2024-03-13 01:30:45 +0400

    костыль для фикса крашей (#21)

--- tools/debug_grid.gd
+++ tools/debug_grid.gd
@@ -3,10 +3,11 @@ extends Node2D
 
 @export var map_size: Vector2 = Vector2.ONE * 16
 @export var cell_size: int = 32
+@export var line_opacity: int = 0
 
 func _draw() -> void:
 	for x in range(map_size.x - 1):
-		draw_line(Vector2(cell_size * (x + 1), 0), Vector2(cell_size * (x + 1), map_size.y * cell_size), Color.WHITE, 2)
+		draw_line(Vector2(cell_size * (x + 1), 0), Vector2(cell_size * (x + 1), map_size.y * cell_size), Color(Color.WHITE, line_opacity), 2)
 		
 	for y in range(map_size.y - 1):
-		draw_line(Vector2(0, cell_size * (y + 1)), Vector2(map_size.x * cell_size, cell_size * (y + 1)), Color.WHITE, 2)
+		draw_line(Vector2(0, cell_size * (y + 1)), Vector2(map_size.x * cell_size, cell_size * (y + 1)), Color(Color.WHITE, line_opacity), 2)
