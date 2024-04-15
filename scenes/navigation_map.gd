bc622ecdc4dcaa01adbfea9f9189b85346642194
Egor Konovalov <agegorin@gmail.com>
2024-04-15 10:26:14 +0200

    now the bridge is actually being built (#39)

--- scenes/navigation_map.gd
+++ scenes/navigation_map.gd
@@ -10,10 +10,6 @@ var path_end_position: Vector2i = Vector2i(): set = _set_path_end_position
 var _point_path = []
 var cell_size: int = 32
 
-func _unhandled_input(_event: InputEvent) -> void:
-	if Input.is_key_pressed(KEY_B):
-		buid_bridge()
-		
 func buid_bridge():
 	astar_grid.set_point_solid(Vector2i(13, 30), false)
 	astar_grid.set_point_solid(Vector2i(14, 30), false)
@@ -29,12 +25,12 @@ func _ready():
 	astar_grid.offset = Vector2(32, 32)
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
 	astar_grid.update()
-	
+
 	astar_grid.fill_solid_region(Rect2i(0, 0, map_size.x, map_size.y), true)
-	
+
 	for cell in get_used_cells(0):
 		astar_grid.set_point_solid(cell, false)
-		
+
 	calculate_obstacles()
 
 func calculate_obstacles() -> void:
@@ -42,14 +38,14 @@ func calculate_obstacles() -> void:
 		var collision: CollisionShape2D = obstacle.get_node('Collision')
 		assert(collision, 'No collision in %s node' % obstacle.get_name())
 		assert(collision.shape, 'No collision shape in %s node' % obstacle.get_name())
-		
-		var collision_start: Vector2i = local_to_map(to_local(collision.global_position + collision.shape.get_rect().position)) 
+
+		var collision_start: Vector2i = local_to_map(to_local(collision.global_position + collision.shape.get_rect().position))
 		var collision_end: Vector2i = local_to_map(to_local(collision.global_position + collision.shape.get_rect().end))
-		
+
 		for x in range(collision_start.x, collision_end.x + 1):
 			for y in range(collision_start.y, collision_end.y + 1):
 				astar_grid.set_point_solid(Vector2i(x, y), true)
-				
+
 func get_astar_path(world_start, world_end) -> Array[Vector2]:
 	path_start_position = local_to_map(to_local(world_start))
 	path_end_position = local_to_map(to_local(world_end))
@@ -57,12 +53,12 @@ func get_astar_path(world_start, world_end) -> Array[Vector2]:
 	var path_world: Array[Vector2] = []
 	for point in _point_path:
 		path_world.append(point)
-		
+
 	return path_world
-	
+
 func _recalculate_path():
 	_point_path = astar_grid.get_point_path(path_start_position, path_end_position)
-	
+
 func is_outside_map_bounds(point: Vector2i) -> bool:
 	return point.x < 0 or point.y < 0 or point.x >= map_size.x or point.y >= map_size.y
 
@@ -85,4 +81,4 @@ func _set_path_end_position(value) -> void:
 	path_end_position = value
 	if path_start_position != value:
 		_recalculate_path()
-		
+
