3b2b55b042a97c8870b5c4f0907429a84a4d8010
egor agegorin <agegorin@gmail.com>
2024-04-26 18:46:42 +0200

    add popup logic

--- global/game_instance.gd
+++ global/game_instance.gd
@@ -1,8 +1,9 @@
 extends Node
 
-@onready var player: Player = get_tree().get_root().get_node('Main/Player') 
+@onready var player: Player = get_tree().get_root().get_node('Main/Player')
 @onready var game_director: GameDirector = get_tree().get_root().get_node('Main/GameDirector')
-@onready var main : Node2D = get_tree().get_root().get_node('Main')
+@onready var main: Node2D = get_tree().get_root().get_node('Main')
+@onready var hud: CanvasLayer = get_tree().get_root().get_node('Main/HUD');
 
 var wood_amount := 0
 var gold_amount := 0
@@ -10,7 +11,7 @@ var meat_amount := 0
 
 func _ready() -> void:
 	player.dead.connect(func(): print('player is dead'))
-	
+
 func get_resource(type, amount):
 	match type:
 		Globals.ResourceType.GOLD_ORE:
@@ -33,3 +34,6 @@ func spend_resource(type, amount):
 			wood_amount -= amount
 		Globals.ResourceType.MEAT:
 			meat_amount -= amount
+
+func show_build_popup(pos: Vector2i, requires: Array[int]):
+	hud.show_build_popup(pos, requires);
