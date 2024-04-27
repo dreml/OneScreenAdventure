5409bed8cb4a6438e64f4295e93b418479acc379
Egor Konovalov <agegorin@gmail.com>
2024-04-27 10:10:27 +0200

    [OSA-35] Добавление попапа строительства (#38)

--- objects/buildings/building.gd
+++ objects/buildings/building.gd
@@ -12,6 +12,7 @@ var _ANIMATIONS_BY_STATES = {
 }
 
 @export var gold_requires: int
+@export var meat_requires: int
 @export var wood_requires: int
 @export var global_rect_adjustment: Vector2
 
@@ -23,6 +24,9 @@ var _ANIMATIONS_BY_STATES = {
 var _state = State.DESTROYED
 
 func start_building() -> bool:
+
+	GameInstance.show_build_popup(self.get_position(), [wood_requires, meat_requires, gold_requires]);
+
 	if !_is_enough_resources() && !_is_constructed():
 		animation_player.play(ANIMATION_NAME_TEMPLATE % "construction_forbidden")
 
@@ -31,12 +35,12 @@ func start_building() -> bool:
 
 	resources_spent.gold = gold_requires
 	resources_spent.wood = wood_requires
-	
+
 	GameInstance.spend_resource(Globals.ResourceType.WOOD, wood_requires)
 	GameInstance.spend_resource(Globals.ResourceType.GOLD_ORE, gold_requires)
-	
+
 	_set_state(State.CONSTRUCTION)
-	
+
 	return true
 
 func build(hp_to_add: int) -> void:
@@ -50,10 +54,10 @@ func get_rect_global() -> Rect2:
 	result.position = global_position + collision_shape.position + global_rect_adjustment
 
 	return result
-	
+
 func get_entrance():
 	var rect = get_rect_global()
-	
+
 	return global_position + Vector2(0, rect.size.y * 2)
 
 func _set_state(state: State):
@@ -69,12 +73,12 @@ func _on_destroyed():
 
 func _can_be_built():
 	return !_is_constructed() && _is_enough_resources()
-	
+
 func _is_constructed():
 	return _state != State.DESTROYED
 
 func _is_enough_resources():
 	var is_enough_wood = GameInstance.wood_amount >= wood_requires
 	var is_enough_gold = GameInstance.gold_amount >= gold_requires
-	
+
 	return is_enough_gold && is_enough_wood
--- ui/hud.gd
+++ ui/hud.gd
@@ -15,3 +15,15 @@ func _on_build_bridge_button_pressed():
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_B):
 		_on_build_bridge_button_pressed()
+
+func show_build_popup(pos: Vector2i, reqires: Array[int]):
+	# У Window, которым является BuildPopup нет возможности задать какой-либо AnchorPoint, поэтому приходится считать тут
+	$BuildPopup.set_position(Vector2i(pos.x - $BuildPopup.get_size().x / 2, pos.y - $BuildPopup.get_size().y - 20))
+	get_node("BuildPopup/Control/PanelContainer/GridContainer/WoodCount/WoodRequired").text = str(reqires[0]);
+	get_node("BuildPopup/Control/PanelContainer/GridContainer/MeatCount/MeatRequired").text = str(reqires[1]);
+	get_node("BuildPopup/Control/PanelContainer/GridContainer/GoldCount/GoldRequired").text = str(reqires[2]);
+	$BuildPopup.show();
+
+
+func _on_build_confirm():
+	$BuildPopup.hide();
