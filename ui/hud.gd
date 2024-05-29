54638794c4eb6d9b096b0ef3015fd6a55ec89c22
egor agegorin <agegorin@gmail.com>
2024-05-29 22:36:53 +0200

    disabled состояние кнопки стройки и красный цвет нехватающих ресурсов

--- ui/hud.gd
+++ ui/hud.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
 
 @onready var build_popup = $BuildPopup
-@onready var wood_required = $BuildPopup/Control/PanelContainer/GridContainer/WoodCount/WoodRequired
-@onready var meat_required = $BuildPopup/Control/PanelContainer/GridContainer/MeatCount/MeatRequired
-@onready var gold_required = $BuildPopup/Control/PanelContainer/GridContainer/GoldCount/GoldRequired
-@onready var build_button = $BuildPopup/Control/PanelContainer/Button
+@onready var wood_required = $BuildPopup/Control/PanelContainer/VBoxContainer/GridContainer/WoodCount/WoodRequired
+@onready var meat_required = $BuildPopup/Control/PanelContainer/VBoxContainer/GridContainer/MeatCount/MeatRequired
+@onready var gold_required = $BuildPopup/Control/PanelContainer/VBoxContainer/GridContainer/GoldCount/GoldRequired
+@onready var build_button = $BuildPopup/Control/PanelContainer/VBoxContainer/Button
 
 @export var grid: Node2D
 @export var nav: NavigationMap
@@ -28,10 +28,30 @@ func show_build_popup(building: Building):
 	var clampedPosition := desirePosition.clamp(Vector2(POPUP_GAP, POPUP_GAP), viewportSize - Vector2(POPUP_GAP, POPUP_GAP) - popupSize);
 
 	build_popup.set_position(clampedPosition);
-	
+
+	var is_enough_wood = GameInstance.wood_amount >= building.wood_requires
+	var is_enough_meat = GameInstance.meat_amount >= building.meat_requires
+	var is_enough_gold = GameInstance.gold_amount >= building.gold_requires
+
+	if not is_enough_wood:
+		wood_required.add_theme_color_override("default_color", Color(1, 0, 0))
+	else:
+		wood_required.remove_theme_color_override("default_color")
+
+	if not is_enough_meat:
+		meat_required.add_theme_color_override("default_color", Color(1, 0, 0))
+	else:
+		meat_required.remove_theme_color_override("default_color")
+
+	if not is_enough_gold:
+		gold_required.add_theme_color_override("default_color", Color(1, 0, 0))
+	else:
+		gold_required.remove_theme_color_override("default_color")
+
 	wood_required.text = str(building.wood_requires);
 	meat_required.text = str(building.meat_requires);
 	gold_required.text = str(building.gold_requires);
+
 	build_button.disabled = !building.can_be_built();
 	current_building = building;
 	build_popup.show();
