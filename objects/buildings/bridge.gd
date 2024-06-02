0ec5744fd41aa80ff295a103e2ab806fb80c2b28
Egor Konovalov <agegorin@gmail.com>
2024-06-02 19:17:21 +0200

    анимация постройки моста (#50)

--- objects/buildings/bridge.gd
+++ objects/buildings/bridge.gd
@@ -4,6 +4,8 @@ class_name Bridge
 @onready var entrance_point = $entrance_point
 
 func _on_built():
+	animation_player.play("building_animations/construction_end")
+	await animation_player.animation_finished
 	super._on_built()
 	GameInstance.build_bridge()
 	queue_free()
--- scenes/level.gd
+++ scenes/level.gd
@@ -1,10 +1,13 @@
 extends TileMap
 
+@onready var animation_player = $AnimationPlayer
+
 func build_bridge():
 	for layer_id in range(self.get_layers_count()):
 		var layer_name = self.get_layer_name(layer_id)
 		if layer_name == "3-bridge-shadow":
 			self.set_layer_enabled(layer_id, true)
+			animation_player.play("show_bridge")
 		if layer_name == "12-bridge":
 			self.set_layer_enabled(layer_id, true)
 		if layer_name == "12-bridge-broken":
