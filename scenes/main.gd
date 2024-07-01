b8437f136c681a320cb4eef16fc799facc3bb06c
dreml <nik.dreml@gmail.com>
2024-07-01 10:39:38 +0400

    [OSA-58] добавил меню с настройками и панель с ресурсами (#54)

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,13 +1,4 @@
 extends Node2D
 
-@onready var gold_label: Label = $GUI/Resources/Gold/Label
-@onready var wood_label: Label = $GUI/Resources/Wood/Label
-@onready var meat_label: Label = $GUI/Resources/Meat/Label
-
 func _ready():
 	pass
-
-func _process(_delta):
-	gold_label.set_text(str(GameInstance.gold_amount))
-	wood_label.set_text(str(GameInstance.wood_amount))
-	meat_label.set_text(str(GameInstance.meat_amount))
--- ui/sound_off_button.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var _sound = AudioServer.get_bus_index("Master")
-	
-func _on_texture_button_pressed():
-	AudioServer.set_bus_mute(_sound, not AudioServer.is_bus_mute(_sound))
